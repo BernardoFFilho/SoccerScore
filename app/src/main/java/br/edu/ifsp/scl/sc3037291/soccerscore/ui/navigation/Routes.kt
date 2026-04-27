dc713a3a978d891924b3af3de39aea1f3983e4c8
Bernardo_Filho <berna10.fernandes@gmail.com>
2026-04-27 15:48:46 -0300

    feat: add navigation dependency and create Routes object

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3037291/soccerscore/ui/navigation/Routes.kt
@@ -0,0 +1,7 @@
+package br.edu.ifsp.scl.sc3037291.soccerscore.ui.navigation
+
+object Routes {
+    const val CONFIG = "config"
+    const val SUMMARY = "summary/{teamA}/{teamB}/{goalsA}/{goalsB}"
+    const val RESULT = "result/{teamA}/{teamB}/{goalsA}/{goalsB}"
+}
