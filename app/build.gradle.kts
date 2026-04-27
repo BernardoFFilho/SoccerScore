812e753f08c9cb40f8e13187f773dcc33d3af7b4
Bernardo_Filho <berna10.fernandes@gmail.com>
2026-04-27 15:41:25 -0300

    build: configure exact versions for Kotlin 2.2, AGP 9.0.1, Gradle 9.2.1 and API 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "br.edu.ifsp.scl.sc3037291.soccerscore"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "br.edu.ifsp.scl.sc3037291.soccerscore"
@@ -48,6 +44,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
